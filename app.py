d2ab748ba5b64fee4b7a671dce0258479578db28
mcbien <mcbien1122@gmail.com>
2020-10-14 21:37:26 -0500

    Saving homework progress

--- app.py
+++ app.py
@@ -38,11 +38,10 @@ def home():
     return (
         f"Bien: sqlalchemy-challenge<br/><br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
+        f"Precipitation >> /api/v1.0/precipitation<br/>"
+        f"Stations >> /api/v1.0/stations<br/>"
+        f"Tobs>> /api/v1.0/tobs<br/>"
         f"/api/v1.0/start date yyyy-mm-dd<br/>"
-        f"/api/v1.0/end date yyyy-mm-dd<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -65,6 +64,9 @@ def precipitation():
         measurement_dict["prcp"] = dates.prcp
         measurement_data.append(measurement_dict)
 
+    mesurement[row.date - row.prcp
+
+
     # Return the JSON representation of your dictionary
     return jsonify(measurement_data)
 
@@ -75,38 +77,64 @@ def stations():
     session = Session(engine)
 
     # query measurement.date and measurement.prcp for last year
+    stations = session.query(measurement.station,func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
+    
+    # close the session
+    session.close()
+
+    # station_data = []
+    # for station in stations:
+    #     stations_dict = {}
+    #     stations_dict["name"] = station[0]
+    #     stations_dict["count"] = station[1]
+    #     station_data.append(stations_dict)
+
+    # Return the JSON representation of your dictionary
+    #return jsonify(station_data)
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+# open communication session with database
+    session = Session(engine)
+
+    # query measurement.date and measurement.prcp for last year
     stations = session.query(measurement.station,func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc())
     
     # close the session
     session.close()
 
-    station_data = []
-    for station in stations:
-        stations_dict = {}
-        stations_dict[0] = station.station
-        stations_dict[1] = func.count(measurement.station)
-        station_data.append(stations_dict)
-
-    # Return the JSON representation of your dictionary
-    return jsonify(station_data)
 
 
+@app.route("/api/v1.0/temps/<start>")
+@app.route("/api/v1.0/temps/<start>/<end>")
 
+def start(start_date=None, end_date=None)
 
-@app.route("/api/v1.0/tobs")
-def tobs():
-    print("test")
-    return("tobs")
+    ## open communication session with database
+    session = Session(engine)
+    
+    # query measurement.date and measurement.prcp for last year
+    if end_input:
+        results - session.query(func.min(Measuremrnt.tobs).label("min", func.max(Measurement.tobs).label("max"), func.avg(Measurement.tobs).label("avg")/
+        .filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    
+    else:
+        results - session.query(func.min(Measuremrnt.tobs).label("min", func.max(Measurement.tobs).label("max"), func.avg(Measurement.tobs).label("avg")/
+        .filter(Measurement.date >= start_date).all()
 
-# @app.route("/api/v1.0/<start>")
+    # close the session
+    session.close()
 
-#     print("test")
-#     return("test")
+    # for loop to pull results for dates provided
+    temp_data = []
+    for result in results:
+        temp_dict = {}
+        temp_dict["min"] = result.min
+        temp_dict["max"] = result.max
+        temp_dict["avg"] = result.avg
 
-# @app.route("/api/v1.0/<start>/<end>")
-
-#     print("test")
-#     return("test")
 
 if __name__ == "__main__":
     app.run(debug=True)
